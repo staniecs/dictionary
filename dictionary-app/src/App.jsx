4a0ddcab77f832d8132acb1a329b84b33fdfc605
Sarah <145446078+staniecs@users.noreply.github.com>
2026-07-05 18:35:46 +0200

    initial styling and github link

--- dictionary-app/src/App.jsx
+++ dictionary-app/src/App.jsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
 <div className="App">
   <h1>Dictionary</h1>
+
+  <p>This app was coded by <a href="https://github.com/staniecs" target="_blank" rel="noopener noreferrer">Sarah Staniec</a> and is open-sourced.</p>
 </div>
       
        
