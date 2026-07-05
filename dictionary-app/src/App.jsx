9af4b8c49e48b8bf02a60d0f3a0d55671dd2b013
Sarah <145446078+staniecs@users.noreply.github.com>
2026-07-05 18:23:50 +0200

    initial set-up
    removed vite content, imported Bootstrap

---
+++ dictionary-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+<div className="App">
+  <h1>Dictionary</h1>
+</div>
+      
+       
+     
+
+  )
+}
+
