7eee8839ae33d002b677369dfc602ca276213d70
Sarah <145446078+staniecs@users.noreply.github.com>
2026-07-05 20:49:19 +0200

    added dictionary api integration
    imported axios, got search bar set up with API

--- dictionary-app/src/App.jsx
+++ dictionary-app/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
 <div className="App">
-  <h1>Dictionary</h1>
+  <div className="container"> 
+    <h1>Dictionary</h1>
+    <Dictionary />
 
   <p>This app was coded by <a href="https://github.com/staniecs" target="_blank" rel="noopener noreferrer">Sarah Staniec</a> and is open-sourced.</p>
+  </div>
+ 
 </div>
       
        
---
+++ dictionary-app/src/Dictionary.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+    
+    function handleResponse(response) {
+        console.log(response.data.word)
+    }
+
+    function search(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword}...`);
+
+        let apiKey = `b3b36of7f40tfb2fc5ea76728725e80c`;
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+    
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+            </form>
+        </div>
+    )
+}
