d8947573fda4587fa96ff96226d9a18d0b90da4f
Sarah <145446078+staniecs@users.noreply.github.com>
2026-07-09 13:07:21 +0200

    Update Photos.jsx
    fixed typo

--- dictionary-app/src/Photos.jsx
+++ dictionary-app/src/Photos.jsx
@@ -13,7 +13,7 @@ export default function Photos(props) {
                                 <div className="col-4" key={index}>
                                     <a href={photo.src.original} target="_blank" rel="noopener noreferrer"><img src={photo.src.landscape} className="img-fluid" alt={photo.alt}/></a>
                                 </div>)
-                        })};
+                        })}
                     </div>
                 </section>
                </div>
